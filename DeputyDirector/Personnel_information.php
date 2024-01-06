40bc9c23c92f06777dcf9843cf100e5f006e6752
Natthawat Pralangkan <120998129+Natthawat-Pralangkan@users.noreply.github.com>
2024-01-07 01:20:45 +0700

    7/1/2567

---
+++ DeputyDirector/Personnel_information.php
@@ -0,0 +1,42 @@
+<?php include("../servers/connect.php"); ?>
+<?php include("../header.php"); ?>
+<div class="wrapper">
+    <?php include('./navbar/sidebar.php'); ?>
+
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+        <?php include('./navbar/navuser.php'); ?>
+        <script src="./js/Personnel_information.js"></script>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <h2 class="m-0">ข้อมูลบุคลากร</h2>
+                    <ol class="breadcrumb float-sm-end">
+                        <li class="breadcrumb-item"><a href="./home.php">หน้าหลัก</a></li>
+                        <li class="breadcrumb-item active">ข้อมูลบุคลากร</li>
+                    </ol>
+                </div>
+            </div>
+            <a href=""></a>
+        </div>
+        <div class="mx-3 mt-5">
+            <div class="mt-3">
+                <table id="addpersonnelinformation" class="table">
+                    <thead>
+                        <tr>
+                            <th>ลำดับ</th>
+                            <th>รูปภาพ</th>
+                            <th>คำนำหน้า</th>
+                            <th>ชื่อ</th>
+                            <th>นามสกุล</th>
+                            <th>ตำแหน่ง</th>
+                        </tr>
+                    </thead>
+                    <!-- เพิ่มข้อมูลตารางตรงนี้ -->
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("../footer.php") ?>
---
+++ DeputyDirector/home.php
@@ -0,0 +1,78 @@
+<?php include("../header.php"); ?>
+<div class="wrapper">
+    <?php include('./navbar/sidebar.php'); ?>
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+        <?php include('./navbar/navuser.php'); ?>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <h2 class="m-0">หน้าหลัก</h2>
+                </div>
+            </div>
+            <a href=""></a>
+        </div>
+        <div class="content">
+            <div class="row p-5 m-1">
+                <div class="col-12 col-md-4">
+                    <div class="shadow p-3 mb-5 bg-body rounded custom-card-ho">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <i class="fa-solid fa-newspaper" style="font-size: 40px;color: #27a645"></i>
+                            <p style="color : #555555">คำร้องรอการอนุมัติ</p>
+                        </div>
+                        <div class="my-3">
+                            <h1 class="text-center">30</h1>
+                        </div>
+                        <div class="border-top text-center py-3">
+                            <span class="text-center">See all</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="shadow p-3 mb-5 bg-body rounded custom-card-ho">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <i class="fa-solid fa-newspaper" style="font-size: 40px;color: #27a645"></i>
+                            <p style="color : #555555">คำร้องรออนุมัติ</p>
+                        </div>
+                        <div class="my-3">
+                            <h1 class="text-center">40</h1>
+                        </div>
+                        <div class="border-top text-center py-3">
+                            <span class="text-center">See all</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="shadow p-3 mb-5 bg-body rounded custom-card-ho">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <i class="fa-solid fa-newspaper" style="font-size: 40px;color: #27a645"></i>
+                            <p style="color : #555555">คำร้องทั้งหมด</p>
+                        </div>
+                        <div class="my-3">
+                            <h1 class="text-center">40</h1>
+                        </div>
+                        <div class="border-top text-center py-3">
+                            <span class="text-center">See all</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="shadow p-3 mb-5 bg-body rounded custom-card-ho">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <i class="fa-regular fa-id-card" style="font-size: 40px;color: #16a1b8"></i>
+                            <p style="color : #555555">บุคลกรทั้งหมด</p>
+                        </div>
+                        <div class="my-3">
+                            <h1 class="text-center">60</h1>
+                        </div>
+                        <div class="border-top text-center py-3">
+                            <span class="text-center">See all</span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("../footer.php") ?>
